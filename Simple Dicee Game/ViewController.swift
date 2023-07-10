55d47168b324bcf337133fe0b18628229f44e548
MD ARAFAT HOSSAIN AKASH <akash076987@gmail.com>
2023-07-10 12:44:23 +0900

    Initial commit

---
+++ Simple Dicee Game/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Simple Dicee Game
+//
+//  Created by Akash Arafat on 2023/07/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFive")
+        //        diceImageView2.image = #imageLiteral(resourceName: "DiceFive")
+                let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+                diceImageView1.image = diceArray.randomElement()
+                diceImageView2.image = diceArray[Int.random(in: 0...5)]
+    }
+}
+
